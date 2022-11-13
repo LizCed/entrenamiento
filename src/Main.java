7ad12783437c43e11b78677430184543b4587f57
Blanca lizbet Cedillo mendez <1000029915@hexaware.com>
2022-11-13 16:17:23 -0600

    Mi primer archivo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    //Metodos, funciones, variables y construcciones
+    public static void main(String[] args) {
+        //Linea que nos indica una salida a pantalla
+        System.out.println("Este es mi primer codigo");
+    }
+}
